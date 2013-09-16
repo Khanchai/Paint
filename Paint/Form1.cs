2255c96fa1c35d16fd50b1b8acd49f05bdbf9301
khanchai <khanchai.c@ssd.co.th>
2013-09-16 16:44:54 +0700

    Create ColorLine and SizeLine

--- Paint/Form1.cs
+++ Paint/Form1.cs
@@ -12,20 +12,24 @@ namespace Paint
 {
     public partial class Form1 : Form
     {
+        Pen pen;
+
         bool paint = false;
         bool start = true;
         bool line = true;
         bool ellipse = false;
         bool eraser = false;
-        int time = 0;
 
-        Pen pen = new Pen(Color.Black);
+        int time = 0;
+        
         Point point1;
         Point point2;
+        
 
         public Form1()
         {
             InitializeComponent();
+            pen = new Pen(Color.Black, 1);
         }
 
         private void panel1_MouseDown(object sender, MouseEventArgs e)
@@ -39,12 +43,14 @@ namespace Paint
 
         private void panel1_MouseUp(object sender, MouseEventArgs e)
         {
+            
             paint = false;
             start = true;
         }
 
         private void panel1_MouseMove(object sender, MouseEventArgs e)
         {
+
             if (!paint) return;
             if (time == 0 && !start)
             {
@@ -57,7 +63,7 @@ namespace Paint
             if (start)
             {
                 point1 = new Point(e.X,e.Y);
-                point2 = new Point(e.X-1,e.Y);
+                point2 = new Point(e.X,e.Y);
                 start = false;
             }
 
@@ -89,10 +95,31 @@ namespace Paint
             bool line = true;
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void WidthLine2(object sender, EventArgs e)
         {
-
+            pen.Width = 2;
         }
-        
+
+        private void WidthLine3(object sender, EventArgs e)
+        {
+            pen.Width = 3;
+        }
+
+        private void WidthLine4(object sender, EventArgs e)
+        {
+            pen.Width = 4;
+        }
+
+        private void WidthLine5(object sender, EventArgs e)
+        {
+            pen.Width = 5;
+        }
+
+        private void ColorDialog (object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            button6.BackColor = colorDialog1.Color;
+            pen.Color = colorDialog1.Color;
+        }
     }
 }
