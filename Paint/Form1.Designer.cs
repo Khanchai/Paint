98d60a7e6c798335bf42a2a4a05bccfe63f8fed8
khanchai <khanchai.c@ssd.co.th>
2013-09-23 15:59:09 +0700

    Create Rectangle success

--- Paint/Form1.Designer.cs
+++ Paint/Form1.Designer.cs
@@ -29,188 +29,137 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.color = new System.Windows.Forms.Button();
-            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
-            this.button6 = new System.Windows.Forms.Button();
+            this.Pencil_Btn = new System.Windows.Forms.Button();
+            this.Brush_Btn = new System.Windows.Forms.Button();
+            this.Eraser_Btn = new System.Windows.Forms.Button();
+            this.LineLv2_Btn = new System.Windows.Forms.Button();
+            this.LineLv3_Btn = new System.Windows.Forms.Button();
+            this.LineLv4_Btn = new System.Windows.Forms.Button();
+            this.Rectangle_Btn = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.ColorDialog_Btn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // panel1
+            // Pencil_Btn
             // 
-            this.panel1.BackColor = System.Drawing.Color.White;
-            this.panel1.Location = new System.Drawing.Point(12, 12);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(644, 475);
-            this.panel1.TabIndex = 0;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
-            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
-            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
-            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            this.Pencil_Btn.Location = new System.Drawing.Point(12, 12);
+            this.Pencil_Btn.Name = "Pencil_Btn";
+            this.Pencil_Btn.Size = new System.Drawing.Size(75, 23);
+            this.Pencil_Btn.TabIndex = 0;
+            this.Pencil_Btn.Text = "Pencil";
+            this.Pencil_Btn.UseVisualStyleBackColor = true;
+            this.Pencil_Btn.Click += new System.EventHandler(this.Pencil_Click);
             // 
-            // button1
+            // Brush_Btn
             // 
-            this.button1.Location = new System.Drawing.Point(663, 13);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Pencil";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Pencil_Btn);
+            this.Brush_Btn.Location = new System.Drawing.Point(12, 41);
+            this.Brush_Btn.Name = "Brush_Btn";
+            this.Brush_Btn.Size = new System.Drawing.Size(75, 23);
+            this.Brush_Btn.TabIndex = 1;
+            this.Brush_Btn.Text = "Brush";
+            this.Brush_Btn.UseVisualStyleBackColor = true;
+            this.Brush_Btn.Click += new System.EventHandler(this.Brush_Btn_Click);
             // 
-            // button2
+            // Eraser_Btn
             // 
-            this.button2.Location = new System.Drawing.Point(663, 70);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "lineLv2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.WidthLine2_Btn);
+            this.Eraser_Btn.Location = new System.Drawing.Point(13, 71);
+            this.Eraser_Btn.Name = "Eraser_Btn";
+            this.Eraser_Btn.Size = new System.Drawing.Size(75, 23);
+            this.Eraser_Btn.TabIndex = 2;
+            this.Eraser_Btn.Text = "Eraser";
+            this.Eraser_Btn.UseVisualStyleBackColor = true;
+            this.Eraser_Btn.Click += new System.EventHandler(this.Eraser_Btn_Click);
             // 
-            // button3
+            // LineLv2_Btn
             // 
-            this.button3.Location = new System.Drawing.Point(663, 99);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "lineLv3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.WidthLine3_Btn);
+            this.LineLv2_Btn.Location = new System.Drawing.Point(13, 101);
+            this.LineLv2_Btn.Name = "LineLv2_Btn";
+            this.LineLv2_Btn.Size = new System.Drawing.Size(75, 23);
+            this.LineLv2_Btn.TabIndex = 3;
+            this.LineLv2_Btn.Text = "LineLv2";
+            this.LineLv2_Btn.UseVisualStyleBackColor = true;
+            this.LineLv2_Btn.Click += new System.EventHandler(this.LineLv2_Btn_Click);
             // 
-            // button4
+            // LineLv3_Btn
             // 
-            this.button4.Location = new System.Drawing.Point(663, 128);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "lineLv4";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.WidthLine4_Btn);
+            this.LineLv3_Btn.Location = new System.Drawing.Point(12, 131);
+            this.LineLv3_Btn.Name = "LineLv3_Btn";
+            this.LineLv3_Btn.Size = new System.Drawing.Size(75, 23);
+            this.LineLv3_Btn.TabIndex = 4;
+            this.LineLv3_Btn.Text = "LineLv3";
+            this.LineLv3_Btn.UseVisualStyleBackColor = true;
+            this.LineLv3_Btn.Click += new System.EventHandler(this.LineLv3_Btn_Click);
             // 
-            // button5
+            // LineLv4_Btn
             // 
-            this.button5.Location = new System.Drawing.Point(663, 157);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "lineLv5";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.WidthLine5_Btn);
+            this.LineLv4_Btn.Location = new System.Drawing.Point(12, 161);
+            this.LineLv4_Btn.Name = "LineLv4_Btn";
+            this.LineLv4_Btn.Size = new System.Drawing.Size(75, 23);
+            this.LineLv4_Btn.TabIndex = 5;
+            this.LineLv4_Btn.Text = "LineLv4";
+            this.LineLv4_Btn.UseVisualStyleBackColor = true;
+            this.LineLv4_Btn.Click += new System.EventHandler(this.LineLv4_Btn_Click);
             // 
-            // color
+            // Rectangle_Btn
             // 
-            this.color.BackColor = System.Drawing.SystemColors.Control;
-            this.color.Location = new System.Drawing.Point(663, 186);
-            this.color.Name = "color";
-            this.color.Size = new System.Drawing.Size(75, 23);
-            this.color.TabIndex = 6;
-            this.color.Text = "Color";
-            this.color.UseVisualStyleBackColor = false;
-            this.color.Click += new System.EventHandler(this.ColorDialog);
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(663, 215);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
-            this.button6.TabIndex = 7;
-            this.button6.Text = "Rectangle";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.Rectangle_Btn);
+            this.Rectangle_Btn.Location = new System.Drawing.Point(12, 191);
+            this.Rectangle_Btn.Name = "Rectangle_Btn";
+            this.Rectangle_Btn.Size = new System.Drawing.Size(75, 23);
+            this.Rectangle_Btn.TabIndex = 6;
+            this.Rectangle_Btn.Text = "Rectangle";
+            this.Rectangle_Btn.UseVisualStyleBackColor = true;
+            this.Rectangle_Btn.Click += new System.EventHandler(this.Rectangle_Btn_Click);
             // 
             // timer1
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // button7
+            // ColorDialog_Btn
             // 
-            this.button7.Location = new System.Drawing.Point(663, 273);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(75, 23);
-            this.button7.TabIndex = 8;
-            this.button7.Text = "Brush";
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.Brush_Btn);
-            // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(663, 42);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(75, 22);
-            this.button8.TabIndex = 9;
-            this.button8.Text = "Eraser";
-            this.button8.UseVisualStyleBackColor = true;
-            this.button8.Click += new System.EventHandler(this.Eraser_Btn);
-            // 
-            // button9
-            // 
-            this.button9.Location = new System.Drawing.Point(663, 244);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(75, 23);
-            this.button9.TabIndex = 10;
-            this.button9.Text = "Ellipse";
-            this.button9.UseVisualStyleBackColor = true;
-            this.button9.Click += new System.EventHandler(this.button9_Click);
-            // 
-            // button10
-            // 
-            this.button10.Location = new System.Drawing.Point(663, 303);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(75, 23);
-            this.button10.TabIndex = 11;
-            this.button10.Text = "Line";
-            this.button10.UseVisualStyleBackColor = true;
-            this.button10.Click += new System.EventHandler(this.button10_Click);
+            this.ColorDialog_Btn.Location = new System.Drawing.Point(12, 221);
+            this.ColorDialog_Btn.Name = "ColorDialog_Btn";
+            this.ColorDialog_Btn.Size = new System.Drawing.Size(75, 23);
+            this.ColorDialog_Btn.TabIndex = 7;
+            this.ColorDialog_Btn.Text = "ColorDialog";
+            this.ColorDialog_Btn.UseVisualStyleBackColor = true;
+            this.ColorDialog_Btn.Click += new System.EventHandler(this.ColorDialog_Btn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(744, 499);
-            this.Controls.Add(this.button10);
-            this.Controls.Add(this.button9);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.color);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ColorDialog_Btn);
+            this.Controls.Add(this.Rectangle_Btn);
+            this.Controls.Add(this.LineLv4_Btn);
+            this.Controls.Add(this.LineLv3_Btn);
+            this.Controls.Add(this.LineLv2_Btn);
+            this.Controls.Add(this.Eraser_Btn);
+            this.Controls.Add(this.Brush_Btn);
+            this.Controls.Add(this.Pencil_Btn);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.OnMouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.OnMouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnMouseUp);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button color;
-        private System.Windows.Forms.ColorDialog colorDialog1;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button Pencil_Btn;
+        private System.Windows.Forms.Button Brush_Btn;
+        private System.Windows.Forms.Button Eraser_Btn;
+        private System.Windows.Forms.Button LineLv2_Btn;
+        private System.Windows.Forms.Button LineLv3_Btn;
+        private System.Windows.Forms.Button LineLv4_Btn;
+        private System.Windows.Forms.Button Rectangle_Btn;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button ColorDialog_Btn;
+
 
     }
 }
--- Paint/Form1.cs
+++ Paint/Form1.cs
@@ -14,15 +14,15 @@ namespace Paint
 {
     public partial class Form1 : Form
     {
-        Pen pen;
+        Rectangle rect;
         Graphics graphics;
+        bool isDrag = false;
+//        GraphicsState initialState;
+
+        Pen pen;
         SolidBrush solidBrushEraser;
         SolidBrush solidBrush;
         Rectangle rectang;
-        GraphicsState initialState;
-
-        DashStyle dashStyle;
-        HatchStyle hatchStyle;
 
         bool paint = false;
         bool start = true;
@@ -44,164 +44,88 @@ namespace Paint
         Point startPoint;
         Point endPoint;
 
-
-
-        Point startPos;      
-        Point currentPos;    
-
         public Form1()
         {
             InitializeComponent();
             Cursor = Cursors.Cross;
             DoubleBuffered = true;
-           
+            graphics = CreateGraphics();
             pen = new Pen(Color.Black, 1);
             solidBrushEraser = new SolidBrush(Color.White);
             solidBrush = new SolidBrush(Color.Black);
-           
+
             width = 20;
             height = 20;
         }
-
-        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        private void OnMouseDown(object sender, MouseEventArgs e)
         {
             if (paint)
             {
                 start = true;
             }
             paint = true;
+            if (rectangle)
+            {
+                rect = RectangleTools.Draw(pen, this);
+                graphics.DrawRectangle(pen, rect);
+            }
 
         }
 
-        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        private void OnMouseUp(object sender, MouseEventArgs e)
         {
             paint = false;
             start = true;
         }
 
-        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        private void OnMouseMove(object sender, MouseEventArgs e)
         {
-
-            if (paint)
+            if (!paint) return;
+            if (time == 0 && !start)
             {
-                if (time == 0 && !start)
-                {
-                    startPoint = new Point(e.X, e.Y);
-                }
-                if (time == 1 && !start)
-                {
-                    endPoint = startPoint;
-                }
-                if (start)
-                {
-                    startPoint = new Point(e.X-2, e.Y-2);
-                    endPoint = new Point(e.X - 2, e.Y-2);
-                    start = false;
-                }
+                startPoint = new Point(e.X, e.Y);
+            }
+            if (time == 1 && !start)
+            {
+                endPoint = startPoint;
+            }
 
-                graphics = panel1.CreateGraphics();
+            if (start)
+            {
+                startPoint = new Point(e.X, e.Y);
+                endPoint = new Point(e.X - 1, e.Y);
+                start = false;
+            }
 
-                if (eraser)
-                {
-                    graphics.FillRectangle(solidBrushEraser, e.X, e.Y, width, height);
-                    graphics.Dispose();
-                }
+            if (pencil)
+            {
+                graphics.DrawLine(pen, startPoint, endPoint);
+//                graphics.Dispose();
+            }
 
-                if (pencil)
-                {
-                    graphics.DrawLine(pen, startPoint, endPoint);
-                    graphics.Dispose();
-                }
-                if (brush)
-                {
-                    graphics.FillEllipse(solidBrush, e.X, e.Y, width, height);
-                    graphics.Dispose();
-                }
+            if (brush)
+            {
+                graphics.FillEllipse(solidBrush, e.X, e.Y, width, height);
+//                graphics.Dispose();
+            }
 
-                if (rectangle)
-                {
-//                     rectang = new Rectangle(startPos.X, startPos.Y, currentPos.X - startPos.X, currentPos.Y - startPos.Y);
-//                            Math.Min(startPos.X, currentPos.X),
-//                            Math.Min(startPos.Y, currentPos.Y),
-//                            Math.Abs(startPos.X - currentPos.X),
-//                            Math.Abs(startPos.Y - currentPos.Y));
-//                     int xpos = (currentPos.X - startPos.X < startPos.X) ? currentPos.X : startPos.X;
-//                     int ypos = (currentPos.Y - startPos.Y < startPos.Y) ? currentPos.Y : startPos.Y;
-//                     int width = Math.Abs(currentPos.X - startPos.X);
-//                     int height = Math.Abs(currentPos.Y - startPos.Y);
-//                   
-//                    if (startPos.X < currentPos.X)
-//                    {
-//                        drawX = startPos.X;
-//                        width = currentPos.X - startPos.X;
-//                    }
-//                    else
-//                    {
-//                        drawX = currentPos.X;
-//                        width = startPos.X - currentPos.X;
-//                    }
-//
-//                    if (startPos.Y < currentPos.Y)
-//                    {
-//                        drawY = startPos.Y;
-//                        height = currentPos.Y - startPos.Y;
-//                    }
-//                    else
-//                    {
-//                        drawY = currentPos.Y;
-//                        height = startPos.Y - currentPos.Y;
-//                    }
-//                    rectang = new Rectangle(drawX, drawY, width, height);
+            if (eraser)
+            {
+                graphics.FillRectangle(solidBrushEraser, e.X, e.Y, width, height);
+//                graphics.Dispose();
+            }
 
-//                     graphics.DrawRectangle(Pens.Blue, new Rectangle(xpos, ypos, width, height));
-
-                    rectang = RectangleTools.Draw(pen, this);
-                    graphics.DrawRectangle(pen, rectang);
-
-
-                  
-                }
-
-                if (ellipse)
-                {
-                    rectang = new Rectangle(e.X, e.Y, 90, 30);
-                    graphics.DrawEllipse(pen, rectang);
-                }
-
-                if (line)
-                {
-
-                    graphics.DrawLine(pen,startPoint,endPoint);
-                }
-
-                if (!start)
-                {
-                    time++;
-                }
-                if (time > 1)
-                {
-                    time = 0;
-                }
-
-
+            if (!start)
+            {
+                time++;
+            }
+            if (time > 1)
+            {
+                time = 0;
             }
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-        private void Rectangle_Btn(object sender, EventArgs e)
-        {
-            pencil = false;
-            rectangle = true;
-            brush = false;
-            eraser = false;
-            ellipse = false;
-            line = false;
-        }
-
-        private void Pencil_Btn(object sender, EventArgs e)
+        private void Pencil_Click(object sender, EventArgs e)
         {
             pencil = true;
             rectangle = false;
@@ -211,7 +135,7 @@ namespace Paint
             line = false;
         }
 
-        private void Brush_Btn(object sender, EventArgs e)
+        private void Brush_Btn_Click(object sender, EventArgs e)
         {
             pencil = false;
             rectangle = false;
@@ -221,44 +145,7 @@ namespace Paint
             line = false;
         }
 
-        private void WidthLine2_Btn(object sender, EventArgs e)
-        {
-            pen.Width = 2;
-            width = 20;
-            height = 20;
-
-        }
-
-        private void WidthLine3_Btn(object sender, EventArgs e)
-        {
-            pen.Width = 3;
-            width = 30;
-            height = 30;
-        }
-
-        private void WidthLine4_Btn(object sender, EventArgs e)
-        {
-            pen.Width = 4;
-            width = 40;
-            height = 40;
-        }
-
-        private void WidthLine5_Btn(object sender, EventArgs e)
-        {
-            pen.Width = 5;
-            width = 50;
-            height = 50;
-        }
-
-        private void ColorDialog(object sender, EventArgs e)
-        {
-            colorDialog1.ShowDialog();
-            color.BackColor = colorDialog1.Color;
-            pen.Color = colorDialog1.Color;
-            solidBrush.Color = colorDialog1.Color;
-        }
-
-        private void Eraser_Btn(object sender, EventArgs e)
+        private void Eraser_Btn_Click(object sender, EventArgs e)
         {
             pencil = false;
             rectangle = false;
@@ -268,29 +155,48 @@ namespace Paint
             line = false;
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void LineLv2_Btn_Click(object sender, EventArgs e)
         {
-            time++;
+            pen.Width = 2;
+            width = 20;
+            height = 20;
         }
 
-        private void button9_Click(object sender, EventArgs e)
+        private void LineLv3_Btn_Click(object sender, EventArgs e)
         {
-            pencil = false;
-            rectangle = false;
-            brush = false;
-            eraser = false;
-            ellipse = true;
-            line = false;
+            pen.Width = 4;
+            width = 23;
+            height = 23;
         }
 
-        private void button10_Click(object sender, EventArgs e)
+        private void LineLv4_Btn_Click(object sender, EventArgs e)
+        {
+            pen.Width = 6;
+            width = 26;
+            height = 26;
+        }
+
+        private void Rectangle_Btn_Click(object sender, EventArgs e)
         {
             pencil = false;
-            rectangle = false;
+            rectangle = true;
             brush = false;
             eraser = false;
             ellipse = false;
-            line = true;
+            line = false;
+        }
+
+        private void ColorDialog_Btn_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            ColorDialog_Btn.BackColor = colorDialog1.Color;
+            pen.Color = colorDialog1.Color;
+            solidBrush.Color = colorDialog1.Color;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time++;
         }
     }
 }
